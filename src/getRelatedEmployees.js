48a29bdeac1bbcacb95684e0353f92ba79f8fca0
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-17 14:36:44 -0300

    fix: requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.find((employee) => employee.id === id)
+  .managers.length < 2;
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter(({ managers }) => managers.includes(managerId))
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
