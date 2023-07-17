8e44e42be5fdcef91e8f89f336523d36d4deff56
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-17 11:46:51 -0300

    fix: requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => data.employees
-  .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName) || {};
+const getEmployeeByName = (employeeName) => {
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+};
 
 module.exports = getEmployeeByName;
