85732b1cbfd98894cd3c9b283d67d4633904d864
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-17 17:56:54 -0300

    fix: requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => entrants.reduce((obj, { age }) => {
+  const newObj = { ...obj };
+  if (age < 18) {
+    newObj.child += 1;
+  } else if (age >= 18 && age < 50) {
+    newObj.adult += 1;
+  } else {
+    newObj.senior += 1;
+  }
+  return newObj;
+}, { child: 0, senior: 0, adult: 0 });
+
+function result(entrants) {
+  const { child, adult, senior } = countEntrants(entrants);
+  const {
+    child: priceChild,
+    adult: priceAdult,
+    senior: priceSenior,
+  } = data.prices;
+  return child * priceChild + adult * priceAdult + senior * priceSenior;
+}
 
 const calculateEntry = (entrants) => {
   // seu código aqui
+  if (!entrants || entrants.lenght === 0) {
+    return 0;
+  }
+  return result(entrants);
 };
 
 module.exports = { calculateEntry, countEntrants };
