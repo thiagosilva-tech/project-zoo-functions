6bdee2c0d620a11876cff326f77cf1f1bf576b6c
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-17 16:16:33 -0300

    fix: requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const findSpecies = (speciesName) => data.species
+  .find((element) => element.name === speciesName);
+
 const countAnimals = (animal) => {
   // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const { species: speciesName, sex } = animal;
+  const species = findSpecies(speciesName);
+  let { residents } = species;
+  if (sex) {
+    residents = residents.filter((resident) => resident.sex === sex);
+  }
+  return residents.length;
 };
 
 module.exports = countAnimals;
