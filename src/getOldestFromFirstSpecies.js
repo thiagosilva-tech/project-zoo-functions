c3013a7879f9ebb46ad5fc57c04d1ed5e8e6756b
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-18 14:08:52 -0300

    fix: requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,18 @@ const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
   // seu código aqui
+  const employeeResponsibleFor = data.employees
+    .find((employee) => employee.id === id).responsibleFor[0];
+
+  const { name, sex, age } = data.species
+    .find((specie) => specie.id === employeeResponsibleFor).residents
+    .reduce((acc, curr) => {
+      if (!acc || acc.age < curr.age) {
+        return curr;
+      }
+      return acc;
+    });
+  return [name, sex, age];
 };
 
 module.exports = getOldestFromFirstSpecies;
