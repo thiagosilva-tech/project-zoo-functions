29c5421c883bde9fbf3c5adfa793b8ab4def6b87
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-18 13:31:59 -0300

    fix: requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
+function createObjDay(day) {
+  if (day === 'Monday') {
+    return {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  }
+  const { open, close } = data.hours[day];
+  return {
+    officeHour: `Open from ${open}am until ${close}pm`,
+    exhibition: data.species
+      .filter(({ availability }) => availability.includes(day))
+      .map(({ name }) => name),
+  };
+}
+
 const getSchedule = (scheduleTarget) => {
   // seu código aqui
+  const speciesTarget = data.species.find(({ name }) => name === scheduleTarget);
+  if (speciesTarget) {
+    return speciesTarget.availability;
+  }
+
+  if (Object.prototype.hasOwnProperty.call(data.hours, scheduleTarget)) {
+    return {
+      [scheduleTarget]: createObjDay(scheduleTarget),
+    };
+  }
+
+  return {
+    Tuesday: createObjDay('Tuesday'),
+    Wednesday: createObjDay('Wednesday'),
+    Thursday: createObjDay('Thursday'),
+    Friday: createObjDay('Friday'),
+    Saturday: createObjDay('Saturday'),
+    Sunday: createObjDay('Sunday'),
+    Monday: createObjDay('Monday'),
+  };
 };
 
 module.exports = getSchedule;
