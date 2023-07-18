8e909a87b9fce14f76ae48a22c593b2e391adcef
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-18 14:41:00 -0300

    fix: requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
+function createObjEmployee(info) {
+  const findEmployee = data.employees
+    .find(({ firstName, lastName, id }) =>
+      firstName === info.name || lastName === info.name || id === info.id);
+  if (!findEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  const species = data.species.filter(({ id }) => findEmployee.responsibleFor.includes(id));
+  return {
+    id: findEmployee.id,
+    fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
+    species: species.map(({ name }) => name),
+    locations: species.map(({ location }) => location),
+  };
+}
+
+const getEmployeesCoverage = (info) => {
   // seu código aqui
+  if (info) {
+    return createObjEmployee(info);
+  }
+  return data.employees.map(({ id }) => createObjEmployee({ id }));
 };
 
 module.exports = getEmployeesCoverage;
