f1496efc57c0b9efe5a2149912548dd48d53b600
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-07-19 14:05:26 -0300

    fix: requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+function createAnimalOptions(filteredSpecies, { includeNames, sorted, sex }) {
+  if (includeNames) {
+    return filteredSpecies.map(({ name, residents }) => {
+      let animalNames;
+      if (sex) {
+        animalNames = residents
+          .filter((resident) => resident.sex === sex)
+          .map((resident) => resident.name);
+      } else animalNames = residents.map((resident) => resident.name);
+      if (sorted) animalNames.sort();
+      return { [name]: animalNames };
+    });
+  }
+  return filteredSpecies.map(({ name }) => name);
+}
 
+function createAnimal(location, options, species) {
+  const filteredSpecies = species.filter((specie) => specie.location === location);
+  if (options) return createAnimalOptions(filteredSpecies, options);
+  return filteredSpecies.map(({ name }) => name);
+}
+
+function getAnimalMap(options) {
+  // seu código aqui
+  return {
+    NE: createAnimal('NE', options, data.species),
+    NW: createAnimal('NW', options, data.species),
+    SE: createAnimal('SE', options, data.species),
+    SW: createAnimal('SW', options, data.species),
+  };
+}
+
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
